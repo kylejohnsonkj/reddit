1eca7c5cb4ada38b10e4148e2115cfaf07cbf2b2
Kyle Johnson <kyjohnson09@gmail.com>
2017-08-02 09:51:29 -0500

    Initial Commit

---
+++ reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reddit
+//
+//  Created by Kyle Johnson on 8/2/17.
+//  Copyright © 2017 Kyle Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
